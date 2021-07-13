4248102cb98d1b0663ad55df21a643ddadebbf77
Karthik Rajesh <karthikrajesh5@gmail.com>
2021-07-13 20:19:58 +0530

    Expedia Project

---
+++ Expedia Flight/ExpediaFlight/src/test/java/com/ibs/training/ExpediaFlight/ExpediaFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibs.training.ExpediaFlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpediaFlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
