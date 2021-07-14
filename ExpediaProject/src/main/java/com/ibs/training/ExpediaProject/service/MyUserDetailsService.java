2ca26a1368d965f1da044b09bcd94e9fa60831fd
Abhishek <abhishekksd13@gmail.com>
2021-07-14 15:33:51 +0530

    UserDetailsService

--- ExpediaProject/src/main/java/com/ibs/training/ExpediaProject/service/MyUserDetailsService.java
+++ ExpediaProject/src/main/java/com/ibs/training/ExpediaProject/service/MyUserDetailsService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
@@ -16,6 +17,7 @@ public class MyUserDetailsService implements UserDetailsService {
     private UserRepository repo;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
         UserEntity user = repo.findByEmail(email);
