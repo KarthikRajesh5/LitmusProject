f0d648c2e9d7880962a1065ce865e533849d47d5
Karthik Rajesh <karthikrajesh5@gmail.com>
2021-07-28 11:31:53 +0530

    Setting UI

--- ExpediaProject/src/main/java/com/ibs/training/ExpediaProject/controller/LoginController.java
+++ ExpediaProject/src/main/java/com/ibs/training/ExpediaProject/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.ibs.training.ExpediaProject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
@@ -10,7 +12,7 @@ public class LoginController {
     public String home(){
         return "home";
     }
-    @GetMapping("/login")
+    @RequestMapping(value="/login",method = {RequestMethod.GET, RequestMethod.PUT})
     public String loginPage() {
         return "login";
     }
